43c4bed8e47f58562887fcd5579c7bd40ae8ff6f
tetejiro <hy1733505@gmail.com>
2022-11-12 16:51:44 +0900

    登録情報管理画面の修正

--- control/delete.php
+++ control/delete.php
@@ -1,49 +1,46 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
-<meta charset="utf-8">
-<title>しつもん</title>
-<meta name="description" content="しつもんするための便利なツール。しつもん上手になって安心して業務に取り組もう。">
-<meta name="viewport" content="width=device-width,initial-scale=1">
+  <meta charset="utf-8">
+  <title>しつもん</title>
+  <meta name="description" content="しつもんするための便利なツール。しつもん上手になって安心して業務に取り組もう。">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
 
-<!-- css -->
-<link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css">
-<link href="https://fonts.googleapis.com/css?family=Noto+Sans+JP" rel="stylesheet">
-<link rel="stylesheet" href="../css/control.css">
-<link rel="icon" type="image/png" href="../favicon/p-favicon.png">
+  <!-- css -->
+  <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Noto+Sans+JP" rel="stylesheet">
+  <link rel="stylesheet" href="../css/control.css">
+  <link rel="icon" type="image/png" href="../favicon/p-favicon.png">
 </head>
+
 <body>
-<div class="mi">
-<?php
+  <div class="delete">
+    <?php
 
-try
-{
-  $code=$_GET['code'];
+    try {
+      $code = $_GET['code'];
 
-  require_once '../new-db/new-select.php';
-  $SelectDb = new SelectDb();
-  $rec = $SelectDb->selectDb10($code);
-  $name=$rec['name'];
-}
-catch(Exception $e)
-{
-  exit('ただいま障害により大変ご迷惑をおかけしております。');
-}
-?>
-<p>このスタッフを削除してよろしいですか？</p>
-<br>
-スタッフコード<br>
-<?php print $code; ?>
-<br>
-スタッフ名<br>
-<?php print $name; ?>
-<br><br>
+      require_once '../new-db/new-select.php';
+      $SelectDb = new SelectDb();
+      $rec = $SelectDb->selectDb10($code);
+      $name = $rec['name'];
+    } catch (Exception $e) {
+      exit('ただいま障害により大変ご迷惑をおかけしております。');
+    }
+    ?>
+    <p>このスタッフを削除してよろしいですか？</p>
+    <div>スタッフコード</div>
+    <div><?php print $code; ?></div>
+    <div>スタッフ名</div>
+    <div><?php print $name; ?></div>
 
-<form method="post" action="delete-done.php">
-<input type="hidden" name="code" value="<?php print $code; ?>">
-<input type="button" onclick="history.back()" value="戻る">
-<input type="submit" value="OK">
-</form>
-</div>
+    <form method="post" action="delete-done.php">
+      <input type="hidden" name="code" value="<?php print $code; ?>">
+      <input type="submit" value="OK">
+      <input type="button" onclick="history.back()" value="戻る">
+    </form>
+  </div>
 </body>
+
 </html>
--- control/edit.php
+++ control/edit.php
@@ -1,70 +1,67 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
-<meta charset="utf-8">
-<title>しつもん</title>
-<meta name="description" content="しつもんするための便利なツール。しつもん上手になって安心して業務に取り組もう。">
-<meta name="viewport" content="width=device-width,initial-scale=1">
+  <meta charset="utf-8">
+  <title>しつもん</title>
+  <meta name="description" content="しつもんするための便利なツール。しつもん上手になって安心して業務に取り組もう。">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
 
-<!-- css -->
-<link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css">
-<link href="https://fonts.googleapis.com/css?family=Noto+Sans+JP" rel="stylesheet">
-<link rel="stylesheet" href="../css/control.css">
-<link rel="icon" type="image/png" href="../favicon/p-favicon.png">
+  <!-- css -->
+  <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Noto+Sans+JP" rel="stylesheet">
+  <link rel="stylesheet" href="../css/control.css">
+  <link rel="icon" type="image/png" href="../favicon/p-favicon.png">
 </head>
+
 <body>
-<div class="mi">
-<?php
+  <?php
 
-try
-{
-  $code=$_GET['code'];
+  try {
+    $code = $_GET['code'];
 
-  require_once '../new-db/new-select.php';
-  $SelectDb = new SelectDb();
-  $rec = $SelectDb->selectDb11($code);
-  $name=$rec['name'];
-  $mail=$rec['mail'];
-}
-catch(Exception $e)
-{
-  print 'ただいま障害により大変ご迷惑をおかけしております。';
-  exit('<a href="../registration/login.html">ログイン</a>し直してください。');
-}
+    require_once '../new-db/new-select.php';
+    $SelectDb = new SelectDb();
+    $rec = $SelectDb->selectDb11($code);
+    $name = $rec['name'];
+    $mail = $rec['mail'];
+  } catch (Exception $e) {
+    print 'ただいま障害により大変ご迷惑をおかけしております。';
+    exit('<a href="../registration/login.html">ログイン</a>し直してください。');
+  }
 
 
-?>
-
-スタッフ修正<br>
-<br>
-スタッフコード<br>
-<?php print $code; ?>
-<br>
-<br>
-<form method="post" action="edit-check.php">
-<input type="hidden" name="code" value="<?php print $code; ?>">
-スタッフ名<br>
-<input type="text" name="name" class="waku" value="<?php print $name; ?>" required><br><br>
-第
-<select name="year">
-  <option value="1">1</option>
-  <option value="2">2</option>
-  <option value="3">3</option>
-  <option value="4">4</option>
-  <option value="5">5</option>
-  <option value="6">シニア</option>
-</select>
-期 <br><br>
-メールアドレス<br>
-<input type="text" name="email" class="waku" value="<?php print $mail; ?>" required><br><br>
-パスワードを入力してください。<br>
-<input type="password" name="pass" class="waku" required><br><br>
-パスワードをもう1度入力してください。<br>
-<input type="password" name="pass2" class="waku" required><br>
-<br>
-<input type="button" onclick="history.back()" value="戻る">
-<input type="submit" value="OK">
-</form>
-</div>
+  ?>
+  <div class="edit">
+    <h3>スタッフ情報 修正</h3>
+    <div>スタッフコード</div>
+    <?php print $code; ?>
+    <form method="post" action="edit-check.php">
+      <input type="hidden" name="code" value="<?php print $code; ?>">
+      <div>スタッフ名</div>
+      <input type="text" name="name" class="waku" value="<?php print $name; ?>" required>
+      <div>
+        第
+        <select name="year">
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">シニア</option>
+        </select>
+        期
+      </div>
+      <div>メールアドレス</div>
+      <input type="text" name="email" class="waku" value="<?php print $mail; ?>" required>
+      <div>パスワードを入力してください。</div>
+      <input type="password" name="pass" class="waku" required>
+      <div>パスワードをもう1度入力してください。</div>
+      <div class="mgn"><input type="password" name="pass2" class="waku" required></div>
+      <input type="submit" value="OK">
+      <input type="button" onclick="history.back()" value="戻る">
+    </form>
+  </div>
 </body>
+
 </html>
