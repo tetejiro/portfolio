39509b0ed5c20a0b33734e993efbce69dacab637
tetejiro <hy1733505@gmail.com>
2022-12-25 14:28:41 +0900

    head部分をすっきりさせる

--- common.php
+++ common.php
@@ -4,19 +4,28 @@ class Common{
   public function printHead($cssContent) {
     $css = $cssContent;
 
-    print '<!DOCTYPE html>';
-    print '<html lang="ja">';
+    session_start();
+    session_regenerate_id(true);
+    if (isset($_SESSION['login']) == false) {
+      print 'ログインしてください。';
+      print '<a href="../registration/login.html">ログインページへ</a>';
+      exit();
+    } else {
+      // headの記載
+      print '<!DOCTYPE html>';
+      print '<html lang="ja">';
 
-    print '<head>';
-    print '  <meta charset="utf-8">';
-    print '  <title>しつもん</title>';
-    print '  <meta name="viewport" content="width=device-width,initial-scale=1">';
+      print '<head>';
+      print '  <meta charset="utf-8">';
+      print '  <title>しつもん</title>';
+      print '  <meta name="viewport" content="width=device-width,initial-scale=1">';
 
-    // css
-    print '  <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css">';
-    print '  <link rel="stylesheet" href='. $css . '>';
-    print '  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Noto+Sans+JP">';
-    print '  <link rel="icon" type="image/png" href="../favicon/p-favicon.png">';
-    print '</head>';
+      // css
+      print '  <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css">';
+      print '  <link rel="stylesheet" href='. $css . '>';
+      print '  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Noto+Sans+JP">';
+      print '  <link rel="icon" type="image/png" href="../favicon/p-favicon.png">';
+      print '</head>';
+    }
   }
 }
