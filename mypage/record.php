9d7ed3439c8029b57aa353b4386f59aa8a530448
tetejiro <hy1733505@gmail.com>
2023-02-21 23:58:22 +0900

    mylist.cssの修正

--- mypage/record.php
+++ mypage/record.php
@@ -16,61 +16,55 @@
       $rec = $DbQuery->dbQuery('SELECT * FROM mypage_infos WHERE member_code = \'' . $honnin . '\'');
       $count = count($rec);
       if (empty($rec)) {
-        print '<div class=center>';
+        print '<div class=subtitle>';
         print 'まだしつもん・ほうれんそうをしていません。';
         print '<p><a href="../mypage/mypage.php?code=' . $_SESSION['code'] . '">もどる</a></p>';
         print '</div>';
       } else {
       for ($i = $count - 1; 0 <= $i; $i--) {
       ?>
-        <div class="zentai">
-          <div class="zentai2">
+        <div class="records">
+          <div class="record">
             <p><?php print $i + 1; ?></p>
-            <div class="zentai3">
-              <table>
-                <tr>
-                  <th>記録時間</th>
-                  <td><?php print mb_substr($rec[$i]['created_at'], 0, 16); ?></td>
-                </tr>
-                <tr>
-                  <th>内容</th>
-                  <td><?php print $rec[$i]['task']; ?></td>
-                </tr>
-                <tr>
-                  <th>開始時間</th>
-                  <td><?php print $rec[$i]['bytime1_1'] . '時' . $rec[$i]['bytime1_2'] . '分'; ?></td>
-                </tr>
-                <tr>
-                  <th>終了時間</th>
-                  <td><?php print $rec[$i]['bytime2_1'] . '時' . $rec[$i]['bytime2_2'] . '分'; ?></td>
-                </tr>
-                <?php
-                $start = new DateTimeImmutable($rec[$i]['bytime1_1'] . ':' . $rec[$i]['bytime1_2']);
-                $stop = new DateTimeImmutable($rec[$i]['bytime2_1'] . ':' . $rec[$i]['bytime2_2']);
-                $interval = $stop->diff($start);
-                ?>
-                <tr>
-                  <th>所要時間</th>
-                  <td><?php print $interval->format("%h時間%i分"); ?></td>
-                </tr>
-                <tr>
-                  <th>当時の気分</th>
-                  <td><?php print $rec[$i]['emotion']; ?></td>
-                </tr>
-              </table>
-    </form>
-    </div>
-    <!--zentai3-->
-    </div>
-    <!--zentai2-->
+            <table>
+              <tr>
+                <th>記録時間</th>
+                <td><?php print mb_substr($rec[$i]['created_at'], 0, 16); ?></td>
+              </tr>
+              <tr>
+                <th>内容</th>
+                <td><?php print $rec[$i]['task']; ?></td>
+              </tr>
+              <tr>
+                <th>開始時間</th>
+                <td><?php print $rec[$i]['bytime1_1'] . '時' . $rec[$i]['bytime1_2'] . '分'; ?></td>
+              </tr>
+              <tr>
+                <th>終了時間</th>
+                <td><?php print $rec[$i]['bytime2_1'] . '時' . $rec[$i]['bytime2_2'] . '分'; ?></td>
+              </tr>
+              <?php
+              $start = new DateTimeImmutable($rec[$i]['bytime1_1'] . ':' . $rec[$i]['bytime1_2']);
+              $stop = new DateTimeImmutable($rec[$i]['bytime2_1'] . ':' . $rec[$i]['bytime2_2']);
+              $interval = $stop->diff($start);
+              ?>
+              <tr>
+                <th>所要時間</th>
+                <td><?php print $interval->format("%h時間%i分"); ?></td>
+              </tr>
+              <tr>
+                <th>当時の気分</th>
+                <td><?php print $rec[$i]['emotion']; ?></td>
+              </tr>
+            </table>
+          </div>
   <?php
       }
       print '<input type="button" onclick="history.back()" value="もどる">';
     }
   ?>
-  </div>
-  <!--zentai-->
-  </form>
+        </div>
+    </form>
   </body>
 
   </html>
--- mypage/shitsumon-list.php
+++ mypage/shitsumon-list.php
@@ -16,10 +16,10 @@
             SELECT name FROM members WHERE code = \'' . $_GET['code'] . '\'
           ')[0];
 
-          print '<p class="center">'.$opponent['name'].' さんへの質問一覧</p>';
+          print '<p class="subtitle">'.$opponent['name'].' さんへの質問一覧</p>';
         }
       ?>
-      <div class="zentai">
+      <div class="records">
         <?php
 
           // 他の人のマイリストの場合、自分から他の人への質問のみを表示する。
@@ -35,7 +35,7 @@
           );
 
           if (empty($selectedQuestion)) {
-            print '<div class=center>';
+            print '<div class=subtitle>';
             print 'まだしつもん・ほうれんそうをしていません。';
             print '<p><a href="../mypage/mypage.php?code=' . $_SESSION['code'] . '">もどる</a></p>';
             print '</div>';
@@ -46,51 +46,47 @@
             $count = count($selectedQuestion);
 
             for ($i = $count - 1; 0 <= $i; $i--) { ?>
-          <div class="zentai2">
+          <div class="record">
             <p><?php print $i + 1; ?></p>
-            <div class="zentai3">
-              <table>
-                <tr>
-                  <th>時間</th>
-                  <td><?php print mb_substr($selectedQuestion[$i]['created_at'], 0, 16); ?></td>
-                </tr>
-                <?php
-                $target_member_codes = $selectedQuestion[$i]['target_member_code'];
-                $aite = $DbQuery->dbQuery('
-                  SELECT name FROM members
-                  WHERE code = '.$target_member_codes
-                );
-                // 退会済みユーザ
-                empty($aite) ? $aite[0]['name'] = '退会済みユーザ' : ''; ?>
-                <tr>
-                  <th>質問相手</th>
-                  <td><?php print $aite[0]['name']; ?> さん</td>
-                </tr>
-                <tr>
-                  <th>件名</th>
-                  <td><?php print $selectedQuestion[$i]['title']; ?></td>
-                </tr>
-                <tr>
-                  <th>依頼したいこと</th>
-                  <td><?php print $selectedQuestion[$i]['purpose']; ?></td>
-                </tr>
-                <tr>
-                  <th>詳細</th>
-                  <td><?php print $selectedQuestion[$i]['detail']; ?></td>
-                </tr>
-                <tr>
-                  <th>考えられる原因</th>
-                  <td><?php print $selectedQuestion[$i]['cause']; ?></td>
-                </tr>
-                <tr>
-                  <th>試したこと・その他</th>
-                  <td><?php print $selectedQuestion[$i]['other']; ?></td>
-                </tr>
-              </table>
-            </div>
-            <!--zentai3-->
+            <table>
+              <tr>
+                <th>時間</th>
+                <td><?php print mb_substr($selectedQuestion[$i]['created_at'], 0, 16); ?></td>
+              </tr>
+              <?php
+              $target_member_codes = $selectedQuestion[$i]['target_member_code'];
+              $aite = $DbQuery->dbQuery('
+                SELECT name FROM members
+                WHERE code = '.$target_member_codes
+              );
+              // 退会済みユーザ
+              empty($aite) ? $aite[0]['name'] = '退会済みユーザ' : ''; ?>
+              <tr>
+                <th>質問相手</th>
+                <td><?php print $aite[0]['name']; ?> さん</td>
+              </tr>
+              <tr>
+                <th>件名</th>
+                <td><?php print $selectedQuestion[$i]['title']; ?></td>
+              </tr>
+              <tr>
+                <th>依頼したいこと</th>
+                <td><?php print $selectedQuestion[$i]['purpose']; ?></td>
+              </tr>
+              <tr>
+                <th>詳細</th>
+                <td><?php print $selectedQuestion[$i]['detail']; ?></td>
+              </tr>
+              <tr>
+                <th>考えられる原因</th>
+                <td><?php print $selectedQuestion[$i]['cause']; ?></td>
+              </tr>
+              <tr>
+                <th>試したこと・その他</th>
+                <td><?php print $selectedQuestion[$i]['other']; ?></td>
+              </tr>
+            </table>
           </div>
-          <!--zentai2-->
         <?php
             }
             print '<input type="button" onclick="history.back()" value="もどる">';
