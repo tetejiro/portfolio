04236487ef15b4e2c077d9ca6bed032f46e7a5b8
tetejiro <hy1733505@gmail.com>
2023-02-23 22:43:10 +0900

    周知事項文字数制限追加

--- registration/insert-announce.php
+++ registration/insert-announce.php
@@ -1,27 +1,28 @@
 <?php
 
-$content=$_POST['content'];
-if(mb_strlen($content)<5)
-{
+// 最小文字数
+if (mb_strlen($_POST['content']) < 5) {
     print '5文字以上は記載してください。';
-    print '<a href="announce.php">もどる</a><br>';
-}
-else
-{
-    try
-    {
+    print '<p><a href="announce.php">もどる</a></p>';
+    print (mb_strlen($_POST['content']) > 61);
+
+// 最大文字数
+} else if (mb_strlen($_POST['content']) > 60) {
+    print '60文字までにしてください。';
+    print '<p><a href="announce.php">もどる</a></p>';
+
+// 上記条件外（インサート可）
+} else {
+
+    try {
         require_once '../new-db/execute-query.php';
         $DbQuery = new DbQuery();
-        $DbQuery->dbQuery('INSERT INTO notices (content) VALUES (\''.$content.'\')');
+        $DbQuery->dbQuery('INSERT INTO notices (content) VALUES (\''.$_POST['content'].'\')');
         header('Location:announce.php');
-    }
-    catch(Exception $e)
-    {
+    } catch(Exception $e) {
         print '障害により記録できませんでした。';
-        print '<a href="index.php">もどる</a><br>';
+        print '<a href="index.php">もどる</a>';
         var_dump($e);
     }
 
-}
-
-?>
+} ?>
