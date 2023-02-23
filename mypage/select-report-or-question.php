1316c7157c2b2c8463841f3d6f62e72af13b610c
tetejiro <hy1733505@gmail.com>
2023-02-23 18:24:18 +0900

    select.cssの修正

--- mypage/select-report-or-question.php
+++ mypage/select-report-or-question.php
@@ -13,12 +13,12 @@
 ?>
 <body>
 <main>
-<div class="bun">
-  <p><?php print $name.'さんに'; ?></p><br>
-  <a href="../shitumon/hokoku.php?code=<?php print $code ?>">報告する</a><br>
-  <a href="../shitumon/shitumon.php?code=<?php print $code ?>">質問を予約する</a><br>
-  <a href="../mypage/member-list.php">メンバーリストへもどる</a>
-</div>
+  <div class="select-list">
+    <p><?php print $name.'さんに'; ?></p>
+    <div><a href="../shitumon/hokoku.php?code=<?php print $code ?>">報告する</a></div>
+    <div><a href="../shitumon/shitumon.php?code=<?php print $code ?>">質問を予約する</a></div>
+    <div><a href="../mypage/member-list.php">メンバーリストへもどる</a></div>
+  </div>
 </main>
 </body>
 </html>
