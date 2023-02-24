1ce2945acb7010dd9e4f5564f7bffac11daec36d
tetejiro <hy1733505@gmail.com>
2023-02-24 22:36:36 +0900

    main.css 修正

--- shitumon/hokoku.php
+++ shitumon/hokoku.php
@@ -1,87 +1,63 @@
 <?php
-session_start();
-session_regenerate_id(true);
-if (isset($_SESSION['login']) == false) {
-        print 'ログインしてください。';
-        print '<a href="../registration/login.html">ログインページへ</a>';
-} else {
-        //このsession[url]の初期化が効かない理由が分からない。
-        $_SESSION['url'] = array();
-        $_SESSION['shitumon'] = 0;
-        $_SESSION['horenso'] = 1;
-        $_SESSION['url'] = $_SESSION['horenso'];
-        $code = $_GET['code'];
 
-        require_once '../new-db/execute-query.php';
-        $DbQuery = new DbQuery();
-        $rec = $DbQuery->dbQuery('
-                SELECT name FROM members WHERE code = \''.$code.'\'
+// headの記載
+require_once('../common.php');
+$cmn = new Common();
+$cmn->printHead('../css/main.css');
+
+$_SESSION['url'] = array(); // なぜか初期化が効かない。
+$_SESSION['shitumon'] = 0;
+$_SESSION['horenso'] = 1;
+$_SESSION['url'] = $_SESSION['horenso'];
+$code = $_GET['code'];
+
+require_once '../new-db/execute-query.php';
+$DbQuery = new DbQuery();
+$rec = $DbQuery->dbQuery('
+                SELECT name FROM members WHERE code = \'' . $code . '\'
         ');
-        $name = $rec[0]['name'];
 ?>
-        <!DOCTYPE html>
-        <html lang="ja">
 
-        <head>
-                <meta charset="utf-8">
-                <title>しつもん</title>
-                <meta name="viewport" content="width=device-width,initial-scale=1">
+<body style="height: 100vh;">
 
-                <!-- css -->
-                <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css">
-                <link rel="stylesheet" href="../css/main.css">
-                <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Noto+Sans+JP">
-                <link rel="icon" type="image/png" href="../favicon/p-favicon.png">
-        </head>
+        <div class="title-container">
+                <img src="../favicon/p-favicon.png">
+                <h2>check</h2>
+                <p><?php print $rec[0]['name'] . 'さんへ報告'; ?></p>
+        </div>
 
-        <body>
-                <div class="ue">
-                        <div class="ue1">
-                                <img src="../favicon/p-favicon.png">
-                                <h2>check</h2>
-                                <p><?php print $name . 'さんへ報告'; ?></p>
-                        </div><!--
-                        <div class="ue2">
-                                <ul>
-                                        <li>答えが知りたいのか、それとも考え方が知りたいのか整理できましたか？</li>
-                                        <li>聞くべき人はその人ですか？</li>
-                                        <li>質問が抽象的すぎませんか？</li>
-                                        <li>分からないことは自分で調べましたか？</li>
-                                        <li>注意書きは読みましたか？</li>
-                                        <li>相手のお話を素直に受け入れる準備はできていますか？</li>
-                                </ul>
-                        </div>-->
-                </div>
-                <form action="insert-branch.php" method="post">
-                        <div class="shitumon">
-                                <div class="goal">
-                                        件名（必須）<br>
-                                        <textarea name="title" rows="10" cols="35" placeholder="※～に関しての報告です。" required></textarea>
-                                </div>
-                                <div class="situation">
-                                        詳細（必須）<br>
-                                        <textarea name="detail" rows="10" cols="35" placeholder="※到達点とのギャップ・問題・報告事項・相談内容など" required></textarea>
-                                </div>
-                                <div class="sonota">
-                                        その他 <br>
-                                        <textarea name="try" rows="10" cols="35" placeholder="※～さんにも報告済みです。"></textarea>
-                                </div>
-                        </div>
-                        <div class="henshin">
-                                返答要・不要（必須）<br><br>
-                                        <label><input type="radio" name="rsvp" value="必要" required>必要</label>
-                                        <label><input type="radio" name="rsvp" value="不要">不要</label>
+        <form action="insert-branch.php" method="post">
+
+                <div class="textarea-container">
+
+                        <div>
+                                <p>件名（必須）</p>
+                                <textarea name="title" rows="10" cols="35" placeholder="※～に関しての報告です。" required></textarea>
                         </div>
 
-                        <?php $code = $_GET['code']; ?>
-                        <input type="hidden" name="code" value="<?php print $code; ?>">
+                        <div>
+                                <p>詳細（必須）</p>
+                                <textarea name="detail" rows="10" cols="35" placeholder="※到達点とのギャップ・問題・報告事項・相談内容など" required></textarea>
+                        </div>
 
-                        <div class="menu">
-                                <input type="submit" value="保存してメールを送る">
-                </form>
-                <a href="../mypage/select-report-or-question.php?code=<?php print $code; ?>">もどる</a>
+                        <div>
+                                <p>その他 </p>
+                                <textarea name="other" rows="10" cols="35" placeholder="※～さんにも報告済みです。"></textarea>
+                        </div>
+
                 </div>
-        </body>
-<?php
-}
-?>
+
+                <div class="rsvp">
+                        <p>返答要・不要（必須）</p>
+                        <label><input type="radio" name="rsvp" value="必要" required>必要</label>
+                        <label><input type="radio" name="rsvp" value="不要">不要</label>
+                </div>
+
+                <input type="hidden" name="code" value="<?php print $_GET['code']; ?>">
+
+                <div class="menu">
+                        <input type="submit" value="保存してメールを送る">
+                        <a href="../mypage/select-report-or-question.php?code=<?php print $_GET['code']; ?>">もどる</a>
+                </div>
+        </form>
+</body>
--- shitumon/shitumon.php
+++ shitumon/shitumon.php
@@ -1,88 +1,80 @@
 <?php
-session_start();
-session_regenerate_id(true);
-if(isset($_SESSION['login'])==false)
-{
-    print 'ログインしてください。';
-    print '<a href="../registration/login.html">ログインページへ</a>';
-}
-else
-{
-    //このsessionの初期化ができないのはなんで？
-    $_SESSION['url']=array();
-    $_SESSION['horenso']=0;
-    $_SESSION['shitumon']=1;
-    $_SESSION['url']=$_SESSION['shitumon'];
-    //質問相手のコード
-    $code=$_GET['code'];
 
-    require_once '../new-db/execute-query.php';
-    $DbQuery = new DbQuery();
-    $rec = $DbQuery->dbQuery('
-        SELECT name FROM members WHERE code = \''.$code.'\'
-    ');
-    $name = $rec[0]['name'];
+// headの記載
+require_once('../common.php');
+$cmn = new Common();
+$cmn->printHead('../css/main.css');
+
+$_SESSION['url']=array();// なぜか初期化が効かない。
+$_SESSION['horenso']=0;
+$_SESSION['shitumon']=1;
+$_SESSION['url']=$_SESSION['shitumon'];
+$code=$_GET['code'];//質問相手のコード
+
+require_once '../new-db/execute-query.php';
+$DbQuery = new DbQuery();
+$rec = $DbQuery->dbQuery('
+    SELECT name FROM members WHERE code = \''.$code.'\'
+');
 ?>
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-<meta charset="utf-8">
-<title>しつもん</title>
-<meta name="viewport" content="width=device-width,initial-scale=1">
 
-<!-- css -->
-<link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css">
-<link rel="stylesheet" href="../css/main.css">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Noto+Sans+JP">
-<link rel="icon" type="image/png" href="../favicon/p-favicon.png">
-</head>
 <body>
-    <div class="ue">
-        <div class="ue1">
-            <img src="../favicon/p-favicon.png">
-            <h2>check</h2>
-            <p><?php print $name.'さんへ質問' ?></p>
-        </div>
-        <div class="ue2">
-            <ul>
-                <li>答えが知りたいのか、それとも考え方が知りたいのか整理できましたか？</li>
-                <li>聞くべき人はその人ですか？</li>
-                <li>質問が抽象的すぎませんか？</li>
-                <li>分からないことは自分で調べましたか？</li>
-                <li>注意書きは読みましたか？</li>
-                <li>相手のお話を素直に受け入れる準備はできていますか？</li>
-            </ul>
-        </div><!--ue2-->
-    </div><!--ue-->
+
+    <div class="title-container">
+        <img src="../favicon/p-favicon.png">
+        <h2>check</h2>
+        <p><?php print $rec[0]['name'].'さんへ質問' ?></p>
+    </div>
+
+    <div class="attention">
+        <ul>
+            <li>答えが知りたいのか、それとも考え方が知りたいのか整理できましたか？</li>
+            <li>聞くべき人はその人ですか？</li>
+            <li>質問が抽象的すぎませんか？</li>
+            <li>分からないことは自分で調べましたか？</li>
+            <li>注意書きは読みましたか？</li>
+            <li>相手のお話を素直に受け入れる準備はできていますか？</li>
+        </ul>
+    </div>
+
     <form action="insert-branch.php" method="post">
-        <div class="shitumon">
-            <div class="goal">
-                件名（必須）<br>
+
+        <div class="textarea-container">
+
+            <div>
+                <p>件名（必須）</p>
                 <textarea name="title" rows="10" cols="35" placeholder="※～について" required></textarea>
             </div>
-            <div class="situation">
-                依頼したいこと（必須）<br>
+
+            <div>
+                <p>依頼したいこと（必須）</p>
                 <textarea name="purpose" rows="10" cols="35" placeholder="※～について解決策が知りたいです。" required></textarea>
             </div>
-            <div class="what">
-                詳細<br>
+
+            <div>
+                <p>詳細</p>
                 <textarea name="detail" rows="10" cols="35" placeholder="※～が表示できません。"></textarea>
             </div>
-            <div class="why">
-                考えられる原因<br>
+
+            <div>
+                <p>考えられる原因</p>
                 <textarea name="cause" rows="10" cols="35" placeholder="※データベースにデータを挿入できていないこと。"></textarea>
             </div>
-            <div class="sonota">
-                試してみたこと・その他<br>
-                <textarea name="try" rows="10" cols="35" placeholder="※inputの属性をtimeからnumberに変更しました。"></textarea>
+
+            <div>
+                <p>試してみたこと・その他</p>
+                <textarea name="other" rows="10" cols="35" placeholder="※inputの属性をtimeからnumberに変更しました。"></textarea>
             </div>
+
         </div>
+
         <input type="hidden" name="code" value="<?php print $code; ?>">
+
         <div class="menu">
-                <input type="submit" value="メールを送信する">
-            </form>
-                <a href="../mypage/select-report-or-question.php?code=<?php print $code; ?>">もどる</a>
+            <input type="submit" value="メールを送信する">
+            <a href="../mypage/select-report-or-question.php?code=<?php print $code; ?>">もどる</a>
         </div>
+
+    </form>
+
 </body>
-<?php }
-?>
