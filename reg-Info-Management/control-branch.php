1d6c9197673f183d7ff40d59f711f185356e49b1
tetejiro <hy1733505@gmail.com>
2023-02-27 15:33:22 +0900

    ファイル削除（ng.php）

--- reg-Info-Management/control-branch.php
+++ reg-Info-Management/control-branch.php
@@ -1,24 +1,16 @@
 <?php
 
+// 編集ボタン押下
 if(isset($_POST['edit']) == true)
 {
-  if(isset($_POST['code']) == false)
-  {
-    header('Location:ng.php');
-    exit('編集する人を選択してください。');
-  }
   $code = $_POST['code'];
   header('Location:edit.php?code='.$code);
   exit();
 }
 
+// 削除ボタン押下
 if(isset($_POST['delete']) == true)
 {
-  if(isset($_POST['code']) == false)
-  {
-    header('Location:ng.php');
-    exit('削除する人を選択してください。');
-  }
   $code = $_POST['code'];
   header('Location:delete.php?code='.$code);
   exit();
